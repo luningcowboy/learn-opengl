97bf6349af8240a194700f9ca52a0e9b331f8069
luningcowboy <luningcowboy@gmail.com>
2019-09-12 10:12:51 +0800

    Initial Commit

---
+++ OpenGL-Tuto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL-Tuto
+//
+//  Created by luning on 2019/9/12.
+//  Copyright © 2019 TU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ OpenGL-Tuto/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OpenGL-Tuto
+//
+//  Created by luning on 2019/9/12.
+//  Copyright © 2019 TU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
