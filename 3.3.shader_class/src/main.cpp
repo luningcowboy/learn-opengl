c68c0737b6c1de65f8c7917b94dab802dfad2c30
luningcowboy <luningcowboy@gmail.com>
2019-11-08 21:13:26 +0800

    finish ~/s/l/l/O/3.3.shader_class

--- 3.3.shader_class/src/main.cpp
+++ 3.3.shader_class/src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <learnopengl/shader_s.h>
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 const unsigned int WIDTH = 800;
@@ -37,14 +38,45 @@ int main(){
         return -1;
     }
 
+    Shader ourShader("src/shader.vs", "src/shader.fs");
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),(void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+
+
+
+
     // render loop
     while(!glfwWindowShouldClose(window)){//检测GLFW是否收到退出事件
         // input
         processInput(window);
+
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         // glfw
         glfwSwapBuffers(window);//交换颜色缓冲
         glfwPollEvents();//触发键盘鼠标事件，更新窗口状态
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     // 清除所有之前分配的glfw资源
     glfwTerminate();
 
