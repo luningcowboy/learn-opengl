af2df2b470e635065fca265467c20ac382b2f41a
luningcowboy <luningcowboy@gmail.com>
2019-11-16 10:32:59 +0800

    init 4.2.textures_combined

---
+++ 4.2.textures_combined/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tu/self/learn/learn-opengl/OpenGL-Tuto/4.2.textures_combined/src/main.cpp" "/Users/tu/self/learn/learn-opengl/OpenGL-Tuto/4.2.textures_combined/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks"
+  "configuration"
+  "includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/tu/self/learn/learn-opengl/OpenGL-Tuto/4.2.textures_combined/CMakeFiles/STB_IMAGE.dir/DependInfo.cmake"
+  "/Users/tu/self/learn/learn-opengl/OpenGL-Tuto/4.2.textures_combined/CMakeFiles/GLAD.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 4.2.textures_combined/configuration/root_directory.h
@@ -0,0 +1,1 @@
+const char * logl_root = "/Users/tu/self/learn/learn-opengl/OpenGL-Tuto/4.2.textures_combined";
